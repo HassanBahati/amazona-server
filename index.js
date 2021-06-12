ead1dd9968f16c617d6a8f748bc451e98c737fe8
HassanBahati <mukisabahati@gmail.com>
2021-06-12 15:09:27 +0300

    added data

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// imports 
 const express = require('express')
+const data = require('./data.js')
 
+// instaiting express 
 const app = express();
 
+// root handler 
 app.get('/', (req,res) =>{
     res.send('Server is ready')
 });
 
+app.get('/api/products', (req,res) =>{
+    res.send(data.products);
+})
+
+// setting port
 app.listen(5000, ()=>{
     console.log('Server running at port 5000')
 });
