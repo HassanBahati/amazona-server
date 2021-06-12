18451a72c1fbd292c7e68f67a919a3c24b09233c
HassanBahati <mukisabahati@gmail.com>
2021-06-12 21:31:52 +0300

    fix bug

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-// imports 
-const express = require('express')
-const data = require('./data.js')
-const port = process.env.PORT || 5000
+// imports
+const express = require("express");
+const data = require("./data.js");
+const port = process.env.PORT || 5000;
 
-// instaiting express 
+// instaiting express
 const app = express();
 
-// root handler 
-app.get('/', (req,res) =>{
-    res.send('Server is ready')
+// root handler
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
-// products route    
-app.get('/api/products', (req,res) =>{
-    res.send(data.products);
+// products route
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
-// prod details 
-app.get('/api/products/:id', (req, res) => {
-    const products = data.products.find((x) => x._id === req.params.id );
-    if(product){
-        res.send(products)
-    }else{
-        res.status(404).send({message: 'Product not found'})
-    }
+// prod details
+app.get("/api/products/:id", (req, res) => {
+  const products = data.products.find((x) => x._id === req.params.id);
+  if (products) {
+    res.send(products);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 
 // setting port
-app.listen(port, ()=>{
-    console.log(`Server running at port ${port}`)
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
