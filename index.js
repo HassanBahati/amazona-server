45e828ba6777b7f060e5d209e9af91e5370b29a7
HassanBahati <mukisabahati@gmail.com>
2021-06-12 20:57:06 +0300

    added prod route

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ app.get('/', (req,res) =>{
     res.send('Server is ready')
 });
 
+// products route    
 app.get('/api/products', (req,res) =>{
     res.send(data.products);
-})
+});
+
+// prod details 
+app.get('/api/products/:id', (req, res) => {
+    const products = data.products.find((x) => x._id === req.params.id );
+    if(product){
+        res.send(products)
+    }else{
+        res.status(404).send({message: 'Product not found'})
+    }
+});
 
 // setting port
 app.listen(port, ()=>{
