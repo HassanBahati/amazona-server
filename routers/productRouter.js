cf0d22f1e534e7b0c9118f54258680d6ff7b976f
HassanBahati <mukisabahati@gmail.com>
2021-06-14 01:02:17 +0300

    fix bug

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -5,12 +5,34 @@ const data = require("../data.js");
 
 const productRouter = express.Router();
 
+//get ll prods
+productRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
 productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     // await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
+  })
+);
+
+// get details - should be at bottom
+productRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Product Not Found" });
+    }
   })
 );
 
