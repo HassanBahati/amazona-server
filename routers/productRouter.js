3e7571ece3daf34e1f08af4465c169defbe5fa43
HassanBahati <mukisabahati@gmail.com>
2021-06-14 00:53:28 +0300

    fix bug

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -8,7 +8,7 @@ const productRouter = express.Router();
 productRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
-    await Product.remove({});
+    // await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
     res.send({ createdProducts });
   })
