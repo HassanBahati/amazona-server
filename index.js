c110390fcea130a03b8b1403a0ab726a8c93614f
HassanBahati <mukisabahati@gmail.com>
2021-06-14 00:50:21 +0300

    claeanedd index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // imports
 const express = require("express");
-const data = require("./data.js");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const userRouter = require("./routers/userRouter.js");
+const productRouter = require("./routers/productRouter.js");
 
 // instantiating express
 const app = express();
-
+ 
 //connct to mongose
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
@@ -19,25 +19,10 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
-{
-}
-
-// products route
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-// prod details
-app.get("/api/products/:id", (req, res) => {
-  const products = data.products.find((x) => x._id === req.params.id);
-  if (products) {
-    res.send(products);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
 
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err });
 });
