bc4570d54c25e8e4685ea7c09914ef934a8bd637
HassanBahati <mukisabahati@gmail.com>
2021-06-14 12:05:24 +0300

    created schmea

---
+++ models/orderModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      qty: { type: Number, required: true },
+      image: { type: String, required: true },
+      price: { type: Number, required: true },
+      //for link
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  shippingAddress: {
+    fullName: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  paymentMethod: { type: String, requierd: true },
+  itemsPrice: { type: Number, required: true },
+  shippingPrice: { type: Number, required: true },
+  taxPrice: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+});
