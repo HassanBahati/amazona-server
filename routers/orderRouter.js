3abefb3486855fae44bccdba6ffc7f22ba084946
HassanBahati <mukisabahati@gmail.com>
2021-06-14 12:15:27 +0300

    created orderrouter

---
+++ routers/orderRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const expressAsyncHandler = require("express-async-handler");
+const Order = require("../models/orderModel");
+
+const orderRouter = express.Router();
+
+orderRouter.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    if (req.body.orderItems.length === 0) {
+      res.status(400).send({ message: "Cart is empty" });
+    } else {
+      const order = new Order({
+        orderItems: req.body.orderItems,
+        shippingAddress: req.body.shippingAddress,
+        paymentMethod: req.body.paymentMethod,
+        itemsPrice: req.body.itemsPrice,
+        shippingPrice: req.body.shippingPrice,
+        taxPrice: req.body.taxPrice,
+        totalPrice: req.body.totalPrice,
+        
+      });
+    }
+  })
+);
