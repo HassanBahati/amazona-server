9b114f13c050d855121373de2c0c0e3a34c1344e
HassanBahati <mukisabahati@gmail.com>
2021-06-14 12:17:54 +0300

    added auth

--- utils.js
+++ utils.js
@@ -8,10 +8,16 @@ const generateToken = (user) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    process.env.JWT_SECRET || 'somethingsecret',
+    process.env.JWT_SECRET || "somethingsecret",
     { expiresIn: "30d" }
   );
 };
 
+const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); //Bearer XXXXXX
+  }
+};
+
 module.exports = generateToken;
- 
